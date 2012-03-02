93a4c0cd2c40757be1ecb7a0d370afca21fc401c
Dmitry Shevchenko <shevchen239@gmail.com>
2012-03-02 18:56:56 +0400

    Let the matrix be determined.

--- matrixGenerator.py
+++ matrixGenerator.py
@@ -1,15 +1,13 @@
 from numpy import matrix
-from random import random, choice 
 
 def wellConditioned(n):
   m = matrix([[0.] * n] * n)
   for i in range(0, n):
-    absSum = 0
     for j in range(0, n):
       if i != j:
-        m[i, j] = random()
-        absSum += abs(m[i, j])
-    m[i, i] = 2 * absSum * choice([-1, 1])
+        m[i, j] = 1. / (j - i) ** 2
+      else:
+        m[i, j] = 2 * (i + 1)
   return m
 
 def illConditioned(n):
