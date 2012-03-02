242ee640a3bef2c0ef0ed5d06c443e38b982e31e
Dmitry Shevchenko <shevchen239@gmail.com>
2012-03-02 18:59:18 +0400

    Now symmetric

--- matrixGenerator.py
+++ matrixGenerator.py
@@ -4,10 +4,7 @@ def wellConditioned(n):
   m = matrix([[0.] * n] * n)
   for i in range(0, n):
     for j in range(0, n):
-      if i != j:
-        m[i, j] = 1. / (j - i) ** 2
-      else:
-        m[i, j] = 2 * (i + 1)
+      m[i, j] = (-0.5) ** abs(j - i) 
   return m
 
 def illConditioned(n):
