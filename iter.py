a6a1bfcca3580d2f0a4aa41f771985983ad37e47
Eugene Luckyanets <leugenea@gmail.com>
2012-03-03 23:59:38 +0400

    idMatrix method

--- iter.py
+++ iter.py
@@ -2,6 +2,12 @@ from copy import copy, deepcopy
 from numpy import matrix, shape, inf, zeros
 from utils import checkSize
 
+def idMatrix(n):
+	a = zeros(n,n)
+	for i in range(n):
+		a[i][i] = 1;
+	return a
+
 def iter(a, b, appr):
 	a = deepcopy(a)
 	b = deepcopy(b)
