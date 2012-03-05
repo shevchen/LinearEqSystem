d16d4cd88c2e12ae26c61ecec5c08ea83caa6105
Dmitry Shevchenko <shevchen239@gmail.com>
2012-03-05 23:22:43 +0400

    minor changes

--- simpleIter.py
+++ simpleIter.py
@@ -1,24 +1,22 @@
-from copy import copy, deepcopy
-from numpy import matrix, shape, inf, zeros, nonzero
+from copy import deepcopy
+from numpy import matrix, shape, inf 
 from utils import *
 from numpy.linalg import norm
 
 def simpleIter(a, b, eps = 1e-10):
-	a = deepcopy(a)
-	b = deepcopy(b)
-	#appr = deepcopy(appr)
-	if not checkSize(a, b):
-		raise Exception("Invalid operands")
-	#if not checkSize(a, appr):
-	#	raise Exception("Invalid operands, wrong size of approximation")
-	if eps == 0:
-		raise Exception("Invalid operands, eps should be not != 0")
-	n = shape(a)[0]
-	a += identityMatrix(n)
-	appr_old = matrix([[inf]] * n)
-	appr = zeroTerm(n)
-	while norm(appr_old - appr) > eps:
-                print appr
-		appr_old = appr
-		appr = a * appr - b
-	return appr
+  a = deepcopy(a)
+  b = deepcopy(b)
+  if not checkSize(a, b):
+    raise Exception("Invalid operands")
+  n = shape(a)[0]
+  a += identityMatrix(n)
+  x = zeroTerm(n)
+  xOld = matrix([[inf]] * n)
+  iters = 0
+  while norm(xOld - x) > eps:
+    iters += 1
+    #print x 
+    xOld = x
+    x = a * x - b
+  print "Iterations in simple iterations method:", iters
+  return x
