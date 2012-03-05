5380d56aefff52e0b536d382a93f3e9449e52bce
Dmitry Shevchenko <shevchen239@gmail.com>
2012-03-05 23:32:42 +0400

    optimized good matrix

--- testGood.py
+++ testGood.py
@@ -1,4 +1,5 @@
 from numpy import *
+from numpy.linalg import norm
 from utils import *
 from gauss import *
 from seidel import *
@@ -7,11 +8,13 @@ from simpleIter import *
 n = 10 
 a = wellConditioned(n)
 b = freeTerm(n)
+print "Norm of the matrix + E:", norm(a + identityMatrix(n))
+print "Condition number of the matrix", norm(a) * norm(a.getI())
 print "Gauss"
 print gauss(a, b)
 print "Seidel, coeff = 1"
 print seidel(a, b)
-print "Simple iterations"
+print "Simple iterations method"
 print simpleIter(a, b)
 print "Correct"
 print linalg.solve(a, b)
--- testIll.py
+++ testIll.py
@@ -7,15 +7,14 @@ from simpleIter import *
 
 n = 10 
 a = illConditioned(n)
-#a2 = wellConditioned(n)
 b = freeTerm(n)
-print "Condition number of ill one", norm(a) * norm(a.getI())
-#print "Condition number of good one", norm(a2) * norm(a2.getI())
+print "Norm of the matrix + E:", norm(a + identityMatrix(n))
+print "Condition number of the matrix", norm(a) * norm(a.getI())
 print "Gauss"
 print gauss(a, b)
 print "Seidel, coeff = 1"
 print seidel(a, b)
 print "Simple iterations method"
-print iteration(a, b)
+#print simpleIter(a, b)
 print "Correct"
 print linalg.solve(a, b)
