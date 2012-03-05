9bfe2c86189e4eee74a7822b2f888db34d8c0e79
Eugene Luckyanets <leugenea@gmail.com>
2012-03-05 22:30:36 +0400

    Added file to test ill conditioned LES

---
+++ testIll.py
@@ -0,0 +1,17 @@
+from numpy import *
+from utils import *
+from gauss import *
+from seidel import *
+from iteration import iteration
+
+n = 10 
+a = illConditioned(n)
+b = freeTerm(n)
+print "Gauss"
+print gauss(a, b)
+print "Seidel, coeff = 1"
+print seidel(a, b, 1e-10)
+print "Iteration method"
+print iteration(a, b, zeroTerm(n), 1e-10)
+print "Correct"
+print linalg.solve(a, b)
