03fe43322cf7bee08e8367a032d15dd43f7c1809
Eugene Luckyanets <leugenea@gmail.com>
2012-03-05 20:12:12 +0400

    Deleted unused function

--- iteration.py
+++ iteration.py
@@ -3,12 +3,6 @@ from numpy import matrix, shape, inf, zeros, nonzero
 from utils import checkSize
 from numpy.linalg import norm
 
-def idMatrix(n):
-	a = zeros((n,n))
-	for i in range(n):
-		a[i][i] = 1;
-	return matrix(a)
-
 def iteration(a, b, appr, eps = 0):
 	a = deepcopy(a)
 	b = deepcopy(b)
@@ -20,7 +14,7 @@ def iteration(a, b, appr, eps = 0):
 	if eps == 0:
 		eps = abs(a[a.nonzero()]).min()/100
 	n = shape(a)[0]
-	a += idMatrix(n)
+	a += identityMatrix(n)
 	appr_old = matrix([[inf]] * n)
 	while norm(appr_old - appr) > eps:
 		appr_old = appr
